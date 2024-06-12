a03b97dc918655b18aebc5e77a6f48466cceb158
muhammad-waqar-uit <muhammadwaqar.business@gmail.com>
2024-06-13 02:12:50 +0500

    working on client table filters

--- src/components/admin/clients/index.tsx
+++ src/components/admin/clients/index.tsx
@@ -1,9 +1,33 @@
 import { ClientTable } from "./component/clienttable";
 
+import React from "react";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { generateData } from "./component/data/data";
+import DataTable from "./component/data-table";
+
+const data = generateData(100);
+
+const columns: ColumnDef<any, any>[] = [
+  {
+    accessorKey: "id",
+    header: "ID",
+  },
+  {
+    accessorKey: "name",
+    header: "Name",
+  },
+  {
+    accessorKey: "value",
+    header: "Value",
+  },
+];
+
 const Client = () => {
   return (
     <div className="bg-background w-full h-full p-10">
-      <ClientTable />
+      {/* <ClientTable /> */}
+      <DataTable data={data} columns={columns} />
     </div>
   );
 };

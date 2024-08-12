878a4034cdd20f5a9a2ae02b44fc570f32a55ef2
Muhammad Waqar <muwaqar.qsol@gmail.com>
2024-08-12 18:53:38 +0500

    updating the exercise

--- src/App.tsx
+++ src/App.tsx
@@ -75,7 +75,7 @@ function App() {
               
               element={<StaffForm />}
             />*/}
-            <Route path="/admin/exercise" element={<Exercise />} />
+            {/* <Route path="/admin/exercise" element={<Exercise />} /> */}
             {/* <Route
               path="/admin/exercise/addexercise"
               
